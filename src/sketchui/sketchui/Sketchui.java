db30da5b20795b5d1c93ea746f669ad2e6740ee8
SpaceCoder <progcoder1@gmail.com>
2025-04-06 09:35:36 +0300

    fix: Update Sketchui.java to enhance functionality

--- src/sketchui/sketchui/Sketchui.java
+++ src/sketchui/sketchui/Sketchui.java
@@ -7,10 +7,12 @@ import processing.core.PApplet;
 public class Sketchui {
   private SketchWindow window;
   private PApplet applet;
-  private List<UIElement> elements;
+  protected static List<UIElement<?>> elements;
 
   private Boolean autoDraw;
   private Boolean isInit = false;
+
+  public Sketchui() {}
 
   public Sketchui(PApplet parent) {
     this.applet = parent;
@@ -24,15 +26,23 @@ public class Sketchui {
   }
 
   public Button createButton(String label, float x, float y, float w, float h) {
-    Button button = new Button(label, x, y, w, h);
-    button.applet = applet;
+    Button button = new Button(applet, label, x, y, w, h);
     elements.add(button);
     window.render(autoDraw, button);
     init();
     return button;
   }
 
-  public void addElement(UIElement element) {
+  public TextField createTextField(String placeholder, float x, float y, float w, float h) {
+    TextField textField = new TextField(applet, placeholder, x, y, w, h);
+    elements.add(textField);
+    window.render(autoDraw, textField);
+    init();
+    return textField;
+  }
+
+  @Deprecated
+  public void addElement(UIElement<?> element) {
     element.applet = applet;
     window.render(autoDraw, element);
   }
