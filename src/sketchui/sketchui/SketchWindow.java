228851c61cded5e0821a89926c1f4004acabff7a
SpaceCoder <progcoder1@gmail.com>
2025-04-06 09:37:25 +0300

    fix: Update SketchWindow for Auto Draw functionality

--- src/sketchui/sketchui/SketchWindow.java
+++ src/sketchui/sketchui/SketchWindow.java
@@ -4,24 +4,33 @@ import java.util.List;
 import processing.core.PApplet;
 
 public class SketchWindow {
-  PApplet window;
+  protected PApplet window;
+  float mouseX, mouseY;
 
   public SketchWindow(PApplet parent) {
     window = parent;
   }
 
-  void render(Boolean autoDraw, UIElement element) {
+  void render(Boolean autoDraw, UIElement<?> element) {
     if (autoDraw && !element.isRegistered) {
       element.isRegistered = true;
+      element.visibility = autoDraw;
       window.registerMethod("draw", element);
+      if (element instanceof Clickable) {
+        window.registerMethod("mouseEvent", element);
+      }
+      if (element instanceof Editable) {
+        window.registerMethod("keyEvent", element);
+      }
+
     } else if (!autoDraw && element.isRegistered) {
       window.unregisterMethod("draw", element);
       element.isRegistered = false;
     }
   }
 
-  void update(Boolean state, Boolean autoDraw, List<UIElement> elements) {
-    for (UIElement e : elements) {
+  void update(Boolean state, Boolean autoDraw, List<UIElement<?>> elements) {
+    for (UIElement<?> e : elements) {
       if (state && autoDraw && !e.isRegistered) {
         window.registerMethod("draw", e);
         e.isRegistered = true;
@@ -34,5 +43,9 @@ public class SketchWindow {
 
   public void keyEvent() {}
 
-  public void mouseEvent() {}
+  public void isMousePressed(UIElement<?> element) {}
+
+  void mouseReleasedEvent() {}
+
+  void setMouseWheelRotation(int theScoller) {}
 }
