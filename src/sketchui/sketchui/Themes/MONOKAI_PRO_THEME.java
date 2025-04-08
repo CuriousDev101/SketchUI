b9cba6129cf7c2ec8e7e1b5292cc98acb85ece3d
Sifen Fiseha <sifen.fiseha@astu.edu.et>
2025-04-08 03:12:46 +0300

    Create MONOKAI_PRO_THEME.java

---
+++ src/sketchui/sketchui/Themes/MONOKAI_PRO_THEME.java
@@ -0,0 +1,24 @@
+package sketchui.Themes;
+
+public class MyMonokaiProTheme {
+    // Base Colors
+    public static final int BG_COLOR       = 0xFF2D2A2E; 
+    public static final int FG_COLOR       = 0xFF403E41; 
+
+    // Accent Colors
+    public static final int ACTIVE_COLOR   = 0xFFAB9DF2; 
+    public static final int HOVER_COLOR    = 0xFFFC9867; 
+
+    // UI Details
+    public static final int BORDER_COLOR   = 0xFF403E41; 
+    public static final int CURSOR_COLOR   = 0xFFFC9867; 
+
+    // Text
+    public static final int TEXT_COLOR     = 0xFFF8F8F2; 
+
+    // Borders
+    public static final float BORDER_RADIUS = 6.0F;
+    public static final float BORDER_WIDTH  = 2.0F;
+
+    public MyMonokaiProTheme() {}
+}
